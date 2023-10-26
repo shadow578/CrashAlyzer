c60bc1a895a23a5d8954a1c11c45b34b5b4e17c4
shadow578 <52449218+shadow578@users.noreply.github.com>
2023-10-26 17:34:22 +0200

    prepare for publish

---
+++ src/cli.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+/**
+ * to make npm link work, we need to start the file with "#!/usr/bin/env node"
+ * but that is not possible in typescript, so we need to create this file as a wrapper...
+ */
+require('./index.js');

625a94c712370512d0fb6eebf5875b72f7df2c4c
shadow578 <52449218+shadow578@users.noreply.github.com>
2023-10-26 17:35:49 +0200

    fix lint failing for cli.js

--- src/cli.js
+++ src/cli.js
@@ -4,4 +4,6 @@
  * to make npm link work, we need to start the file with "#!/usr/bin/env node"
  * but that is not possible in typescript, so we need to create this file as a wrapper...
  */
+
+// eslint-disable-next-line
 require('./index.js');
